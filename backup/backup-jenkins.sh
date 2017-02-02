89f588f551244618ecd6535e2971d21ba9025b08
Tobias Reusing <tobias@reusing.eu>
2017-02-02 18:11:32 +0100

    changed ip

--- backup/backup-jenkins.sh
+++ backup/backup-jenkins.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -a /mnt/nas/Data rsync://192.168.0.166/rsync/ubuntu-nas/
+rsync -a /mnt/nas/Data rsync://192.168.178.69/rsync/ubuntu-nas/
--- backup/backup.sh
+++ backup/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -n "BEGIN:" >> /home/tobi/backup.log
 date >> /home/tobi/backup.log
-rsync -a /mnt/nas/Data rsync://192.168.0.166/rsync/ubuntu-nas/
+rsync -a /mnt/nas/Data rsync://192.168.178.69/rsync/ubuntu-nas/
 echo -n "END:" >> /home/tobi/backup.log
 date >> /home/tobi/backup.log
